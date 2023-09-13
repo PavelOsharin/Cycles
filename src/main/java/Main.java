73601fd282eef4f13eb01221aeea42cb582dc1ec
Pavel Osharin <mr.osharin@mail.ru>
2023-09-13 13:26:27 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import ru.netology.cycles.services.VacationService;
+
+public class Main {
+    public static void main(String[] args) {
+        VacationService service = new VacationService();
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//      int i = service.calculate(income, expenses, threshold);
+//      System.out.println(i);
+    }
+}
